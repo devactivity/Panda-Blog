e25cf8f72f25a7c4f92b0492c84c071ed489733a
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2021-02-06 14:32:58 +0700

    Access sanity data from graphql

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -5,3 +5,39 @@
  */
 
 // You can delete this file if you're not using it
+async function createBlogPostPages (graphql, actions, reporter) {
+  //const { createPage } = actions
+  const result = await graphql(`
+    {
+      allSanityPost(filter: { slug: { current: { ne: null } }}) {
+        edges {
+          node {
+            id
+            publishedAt
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) throw result.errors
+
+  const postEdges = (result.data.allSanityPost || {}) || []
+
+  postEdges.forEach((edge, index) => {
+    const { id, slug = {}, publishedAt } = edge.node
+    const dateSegment = publishedAt.slice(0, 10)
+    const path = `/blog/${dateSegment}/${slug.current}/`
+
+    reporter.info(`Creating blog post page: ${path}`)
+
+    //TODO: call `createPage` here
+  })
+}
+
+exports.createPages = async({ graphql, actions, reporter }) => {
+  await createBlogPostPages(graphql, actions, reporter)
+}
